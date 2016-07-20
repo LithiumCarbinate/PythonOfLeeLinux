bbe77613eb0a76ba71d646a975652969d542e639
liguoli <liguoli@zhizhangyi.com>
2016-07-20 15:42:06 +0800

    备份之前的修改

--- Leelinux.py
+++ Leelinux.py
@@ -1,20 +1,22 @@
 import urllib
 import urllib2
 import sys
+import requests
+import json
 
 class LeeLinux:
     def __init__(self):
         self.url = 'http://jsondata.25pp.com/index.html'
     def getJson(self,pageIndex):
-        data = {'dcType':0,'resType':1,'listType':5,'catId':0,'clFlag':1,'perCount':32,'page':pageIndex}
+        data = {'dcType': 0, 'resType': 1, 'listType': 5, 'catId': 0, 'clFlag': 1, 'perCount': 32, 'page': pageIndex}
         metaData = urllib.urlencode(data)
-        header = {'Host':'jsondata.25pp.com','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0','Accept':'text,/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Language':'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3','Accept-Encoding':'gzip, deflate','Tunnel-Command':'4261433392','Content-Type':'application/json','Referer':'http://ppmac2.25pp.com/zb_pp_v3/index.html','Content-Length':'87','Origin:http':'//ppmac2.25pp.com','Connection':'keep-alive'}
+        header = {'Host':'jsondata.25pp.com', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0', 'Accept': 'text,/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language':'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3', 'Accept-Encoding': 'gzip, deflate', 'Tunnel-Command': 4261433392, 'Content-Type': 'application/json', 'Referer':'http://ppmac2.25pp.com/zb_pp_v3/index.html', 'Content-Length': 87, 'Origin:http': '//ppmac2.25pp.com','Connection': 'keep-alive'}
         metaHeader = urllib.urlencode(header)
-        req = urllib2.Request(self.url,metaData,metaHeader)
+        req = urllib2.Request(self.url, metaData, metaHeader)
         r = urllib2.urlopen(req)
         html=r.read()
         receive_header = r.info()
-        html = html.decode('utf-8','replace').encode(sys.getfilesystemencoding())
+        html = html.decode('utf-8', 'replace').encode(sys.getfilesystemencoding())
         print receive_header
         print '#############'
         print html
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from LeeLinux import *
 leelinux = LeeLinux()
 temp = leelinux.getJson(0)
-print temp
+# print temp
+# for i in range(0, 200):
+    # print(i)
