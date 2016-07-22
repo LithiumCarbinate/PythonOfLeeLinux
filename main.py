e8ecf505281b282e19269145b9ac7293dd7fbbf7
LeeLinux <867962609@qq.com>
2016-07-22 23:26:47 +0800

    工具

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from LeeLinux import *
 
-leelinux = LeeLinux()
-Json = leelinux.getJson(0)
-print Json
-for i in range(0, 200):
-    print(i)
+# print Json
+# for i in range(0, 200):
+#     # print(i)
